19ca8541cc2798ba2ce6d3b7206c2b6a6c46e632
wufengbin2 <wufengbin2@jd.com>
2019-07-24 15:01:27 +0800

    zk client

---
+++ beacon-core/src/main/java/com/beacon/core/register/ZkClient.java
@@ -0,0 +1,36 @@
+package com.beacon.core.register;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+/**
+ * @author fengbin2.wu
+ * @date 2019-07-24 14:49
+ * version 1.0
+ */
+public class ZkClient {
+
+    private static final String NAME_SPACE = "";
+
+    CuratorFramework curatorFramework = CuratorFrameworkFactory.builder()
+            .connectString("")
+            .retryPolicy(new ExponentialBackoffRetry(1000, 5))
+            .namespace(NAME_SPACE)
+            .build();
+
+
+    public void createContainers(String path) {
+        try {
+            curatorFramework.createContainers(path);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void add() {
+        curatorFramework.create();
+    }
+
+
+}
